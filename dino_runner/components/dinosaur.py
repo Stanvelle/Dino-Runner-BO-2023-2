b53c6b56ac94177a9b72733d6de5a9ed61ec69fd
Stanvelle <129458718+Stanvelle@users.noreply.github.com>
2023-03-30 23:48:20 -0400

    Class 1
    Adding Class Dinosaur and Obstacles.

---
+++ dino_runner/components/dinosaur.py
@@ -0,0 +1,14 @@
+from dino_runner.utils.constants import RUNNING, JUMPING, DUCKING
+class Dinosaur:
+    def __init__(self):
+        
+        pass
+
+    def run():
+        pass
+
+    def duck():
+        pass
+
+    def jump():
+        pass
